c7dfb9cc76a0083986490c239a5b5ced2c57ee8c
Sai Sashreek <saisashreekd@gmail.com>
2024-04-29 10:18:43 +0530

    App JS File

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  let counter = 15;
+
+  return (
+    <>
+      <h1>React Course {counter}</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button>Add Counters</button>
+      <button>Remove Counters</button>
+      <p>Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
