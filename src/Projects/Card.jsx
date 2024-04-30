9a1d6ad3f481ce49e65b4c8969e1c78f507d3489
Sai Sashreek <saisashreekd@gmail.com>
2024-04-30 08:52:08 +0530

    Card Component Created

---
+++ src/Projects/Card.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+const Card = ({ userName = "SSD", post = "Engineer" }) => {
+  // console.log(props);
+  return (
+    <>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+          src="https://images.pexels.com/photos/2182863/pexels-photo-2182863.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis,
+              debitis.
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">{userName}</div>
+            <div className="text-slate-700 dark:text-slate-500">
+              {post}
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+    </>
+  );
+};
+
+export default Card;
