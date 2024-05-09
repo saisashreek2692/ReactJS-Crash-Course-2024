3c08ecb59b4cb38e7a85592d1b442c2fee40dfb0
saisashreek <saisashreekd@gmail.com>
2024-05-09 14:58:01 +0530

    Navbar Added to Main Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Menubar from "./UIUX/Menubar";
 import BGChanger from "./Projects/BGChanger";
 import Card from "./Projects/Card";
 import CounterApp from "./Projects/CounterApp";
+import Password from "./Projects/Password";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Menubar />} />
-      <Route path="/counter" element={<CounterApp />} />
-      <Route path="/cards" element={<Card />} />
-      <Route path="/bgchange" element={<BGChanger />} />
-    </Routes>
+    <>
+      <Menubar />
+      <Routes>
+        <Route path="/counter" element={<CounterApp />} />
+        <Route path="/cards" element={<Card />} />
+        <Route path="/bgchange" element={<BGChanger />} />
+        <Route path="/pwd" element={<Password />} />
+      </Routes>
+    </>
   );
 }
 
