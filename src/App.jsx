71b163ed68ecf8032fc40c52c7cc4a600cf648ed
saisashreek <saisashreekd@gmail.com>
2024-05-09 12:45:13 +0530

    App File Updated with Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Menubar from "./UIUX/Menubar";
 import BGChanger from "./Projects/BGChanger";
 import Card from "./Projects/Card";
 import CounterApp from "./Projects/CounterApp";
 
-import { Routes, Route } from "react-router-dom";
-
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<CounterApp />} />
-
+      <Route path="/" element={<Menubar />} />
+      <Route path="/counter" element={<CounterApp />} />
       <Route path="/cards" element={<Card />} />
-
       <Route path="/bgchange" element={<BGChanger />} />
     </Routes>
   );
