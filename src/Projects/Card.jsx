d0d550663ec468de5e6c1dbc2a0ad7c358fede36
saisashreek <saisashreekd@gmail.com>
2024-05-09 12:30:11 +0530

    Cards imported

--- src/Projects/Card.jsx
+++ src/Projects/Card.jsx
@@ -1,31 +1,24 @@
-/* eslint-disable react/prop-types */
-const Card = ({ userName = "SSD", post = "Engineer", imgSrc }) => {
-  // console.log(props);
+import React from "react";
+import Cards from "../UIUX/Cards";
+
+const Card = () => {
   return (
     <>
-      <figure className="mx-auto m-10 md:flex bg-slate-100 rounded-xl p-10 md:p-0 dark:bg-slate-800">
-        <img
-          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-          src={imgSrc}
-          alt=""
-          width="384"
-          height="512"
-        />
-        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-          <blockquote>
-            <p className="text-lg font-medium">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis,
-              debitis.
-            </p>
-          </blockquote>
-          <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400">{userName}</div>
-            <div className="text-slate-700 dark:text-slate-500">
-              {post}
-            </div>
-          </figcaption>
-        </div>
-      </figure>
+      <Cards
+        imgSrc="https://images.pexels.com/photos/2182863/pexels-photo-2182863.jpeg?auto=compress&cs=tinysrgb&w=600"
+        userName="Sai Sashreek D"
+        post="Front End Developer"
+      />
+      <Cards
+        imgSrc="https://images.pexels.com/photos/2182863/pexels-photo-2182863.jpeg?auto=compress&cs=tinysrgb&w=600"
+        userName="Himakshi"
+        post="AI / ML Engineer"
+      />
+      <Cards
+        imgSrc="https://images.pexels.com/photos/2182863/pexels-photo-2182863.jpeg?auto=compress&cs=tinysrgb&w=600"
+        userName="Mounika"
+        post="Deeplearning Architect"
+      />
     </>
   );
 };
