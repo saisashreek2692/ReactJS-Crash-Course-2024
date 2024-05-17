9272c27bebcdaa176c48e99251be398ea90e9104
saisashreek <saisashreekd@gmail.com>
2024-05-17 12:18:48 +0530

    Added New Route as Context

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import Card from "./Projects/Card";
 import CounterApp from "./Projects/CounterApp";
 import Password from "./Projects/Password";
 import Currency from "./Projects/CurrencyCoverter";
+import Contextapp from "./Projects/Contextapp";
 
 function App() {
   return (
     <>
       <Menubar />
       <div className="m-20">
-      <Routes>
-        <Route path="/counter" element={<CounterApp />} />
-        <Route path="/cards" element={<Card />} />
-        <Route path="/bgchange" element={<BGChanger />} />
-        <Route path="/pwd" element={<Password />} />
-        <Route path="/currency" element={<Currency />} />
-      </Routes>
+        <Routes>
+          <Route path="/counter" element={<CounterApp />} />
+          <Route path="/cards" element={<Card />} />
+          <Route path="/bgchange" element={<BGChanger />} />
+          <Route path="/pwd" element={<Password />} />
+          <Route path="/currency" element={<Currency />} />
+          <Route path="/context" element={<Contextapp />} />
+        </Routes>
       </div>
     </>
   );
